f021609ca6244b552a4f322ed381b1c9ad0066f4
Abkariyyah Ahmed <abkariyyah@Mac.lan>
2025-06-06 13:37:13 -0400

    Project 2

--- Loops & Conditionals/string_manipulation.cpp
+++ Loops & Conditionals/string_manipulation.cpp
@@ -3,27 +3,50 @@
 using namespace std;
 
 int main() {
-string fullName = "Abkariyyah Ahmed";
-string slice = fullName.substr(0, 10);
-string slice2 = fullName.substr(11, 5);
+    string fullName;
+    cout << "Enter your full name (First and Last): " << endl;
+    getline(cin, fullName); //to handle inputs with spaces
 
-//first name to uppercase
-for (int i = 0; i < slice.length(); i++) {
-        if (slice[i] >= 'a' && slice[i] <= 'z') {
-            slice[i] = slice[i] - 32;
-        }
-    }
-
-//last name to uppercase
-for (int i = 0; i < slice2.length(); i++) {
-        if (slice2[i] >= 'a' && slice2[i] <= 'z') {
-            slice2[i] = slice2[i] - 32;
-        }
-    }
-
-string transformedName = slice + " " + slice2;
-cout << "Transformed name: " << transformedName << endl;
+    //First name
+    int i = 0; // index to loop over fullName
     
+    while (i < fullName.length() && 
+          ((fullName[i] >= 'a' && fullName[i] <= 'z') || 
+           (fullName[i] >= 'A' && fullName[i] <= 'Z'))) {
 
-return 0;
+         i++; //iterate for every i that is not a space (stops when space is found)
+    }
+    string first = fullName.substr(0, i); //store first name into variable
+    
+    //skip the space
+    while (i < fullName.length() && fullName[i] == ' ') {
+    i++;
+}
+    // Find where last name ends
+    int Last_start = i; // starting index of last name
+    while (i < fullName.length() && 
+          ((fullName[i] >= 'a' && fullName[i] <= 'z') || 
+           (fullName[i] >= 'A' && fullName[i] <= 'Z'))) {
+        
+        i++;
+    }   
+    string last = fullName.substr(Last_start, i - Last_start); //store last name into variable
+
+    //PT 2 - convert first and last into uppercase!
+
+    for (int c = 0; c < first.length(); c++) {
+        if (first[c] >= 'a' && first[c] <= 'z') {
+            first[c] = first[c] - 32;
+    }
+}
+    for (int c = 0; c < last.length(); c++) {
+        if (last[c] >= 'a' && last[c] <= 'z') {
+            last[c] = last[c] - 32;
+    }
+}
+    string transformedname = first + " " + last;
+    cout << "Transformed name: " << transformedname << endl;
+
+    return 0;
+
 }
